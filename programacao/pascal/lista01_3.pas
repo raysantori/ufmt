74fae9198ffdcfac23b9281d11b6ce70017db040
raysantori <contato@raysantori.com>
2023-03-09 13:45:46 -0300

    lista01_3

--- programacao/pascal/lista01_3.pas
+++ programacao/pascal/lista01_3.pas
@@ -3,15 +3,33 @@ program lista01_3;
 // resolvendo os valores de x k+1 com quatro casas decimais e uma solução aceitável para x^2-2=0
 
 var
-    x0, x1, x2, x3, x4 test, k: real;
+    a, b, c, delta, k, x, x1, x2: real;
+    vetor: array[0..4] of integer;
+    index: integer;
+
+    // x0: aproximação inicial
+    // x1: solução aceitável / raiz da equação
+        // test: se atender a condição erro / teste de parada
+
+function getIterativa(params):integer;
+begin
+    getIterativa:= (x*x+2)/(2*x);
+end;
 
 begin // principal
-    x0:= 1;
+    a:= 1;
+    b:= 0;
+    c:= -2;
+    
+    write('Entre com vetor[', index,']:');
+    write('Entre com vetor[', index,']:');
+    write('Entre com vetor[', index,']:');
+    write('Entre com vetor[', index,']:');
 
-    x1:=;
-
-    writeln;
-    writeln(x1);
+    for i := 0 to 4 do
+    begin
+        
+    end;
 
     writeln; writeln('Fim.');
 end.
