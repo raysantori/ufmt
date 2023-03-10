01fdad105c68a1ae2b70881e985473dd7a63fc00
raysantori <contato@raysantori.com>
2023-03-10 13:16:59 -0300

    aula 04_5 & 05

---
+++ programacao/pascal/aula04_5.pas
@@ -0,0 +1,34 @@
+program aula04_5; // soma de matrizes
+
+var 
+	A: array[0..1, 0..2] of real; // 2 linhas e 3 colunas ou 2x3
+	B: array[0..1, 0..2] of real; // 2 linhas e 3 colunas ou 2x3
+	S: array[0..1, 0..2] of real; // 2 linhas e 3 colunas ou 2x3 
+
+	i, j: integer;
+
+begin // principal
+	A[0,0]:= 1; A[0,1]:=2; A[0,2]:=3;	// 1a. linha
+    A[1,0]:= 4; A[1,1]:=5; A[1,2]:=6;	// 2a. linha
+	
+	B[0,0]:= 7; A[0,1]:=8; A[0,2]:=9;	    // 1a. linha
+	B[1,0]:= 10; A[1,1]:=11; A[1,2]:=12;	// 2a. linha
+
+	for i:= 0 to 1 do 
+	begin 
+		for j:= 0 to 2 do
+		begin
+			S[i, j]:= A[i, j] + B[i, j]; 
+		end;
+	end;
+	for i:= 0 to 1 do //-----------
+	begin
+		for j:= 0 to 2 do
+		begin
+			write('S[' ,i,',',j, '] = ', S[i, j]:0:2, ' ');
+			// write(S[i, j]:8:2); //14 colunas sendo 2 para os decimais
+		end;
+		writeln;
+	end;
+	readln;
+end.
---
+++ programacao/pascal/aula05.pas
@@ -0,0 +1,42 @@
+program aula05; // multiplicação de matrizes
+
+var
+  A: array[0..2, 0..1] of real; //3x2
+  B: array[0..1, 0..3] of real; //2x4
+  C: array[0..2, 0..3] of real; //3x4
+
+  i, j, k: integer;
+
+begin // principal
+  A[0,0]:=1;  A[0,1]:=2;
+  A[1,0]:=3;  A[1,1]:=4;
+  A[2,0]:=5;  A[2,1]:=6;
+
+  B[0,0]:=7;   B[0,1]:=8;   B[0,2]:=9;   B[0,3]:=10;
+  B[1,0]:=11;  B[1,1]:=12;  B[1,2]:=13;  B[1,3]:=14;
+
+  for i:= 0 to 2 do
+  begin
+      for k:= 0 to 3 do
+      begin
+          C[i,k]:=0;
+
+          for j:=0 to 1 do
+          begin
+              C[i,k]:= C[i,k] + A[i,j]*B[j,k];
+          end;
+      end;
+  end;
+
+  for i:= 0 to 2 do
+  begin
+      for k:= 0 to 3 do
+      begin
+          write(C[i,k]:8:2, '  ');
+      end;
+
+      writeln;
+  end;
+
+  writeln; write('Fim.'); readln;
+end.
