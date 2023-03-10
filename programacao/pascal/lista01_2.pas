c8e473115baa0e44a0695efd9c7535bbe8f1f88b
raysantori <contato@raysantori.com>
2023-03-10 13:32:04 -0300

    fix lista01_2.pas

--- programacao/pascal/lista01_2.pas
+++ programacao/pascal/lista01_2.pas
@@ -2,28 +2,31 @@ program lista01_2; // descobrir todos o números ímpares entre números naturai
 
 var 
     i, n1, n2: integer;
-    info: char;
 
 begin // principal
-    info:= 'y';
-
-    while (info = 'y') do
+    write('Entre com N1: '); read(n1);
+    write('Entre com N2: '); read(n2); writeln;
+    
+    if n1 < n2 then
     begin
-        write('Entre com N1: '); read(n1);
-        write('Entre com N2: '); read(n2); writeln;
-
-        for i:=n1 to n2 do
+        for i := n1 to n2 do
         begin
-            if ((i mod 2) <> 0) then // cálculo dos números ímpares
+            if ((i mod 2) <> 0) then // calc resto p determinar se é ímpar
             begin
                 write(#9, i);
-            end; 
+            end;
         end;
-        //bug: depois de pedir o y do usuário  ele para o programa :'(
-        //writeln; writeln; writeln('Digite y para continuar.');
-        //readln(info); 
-        writeln;
     end;
-  
-  writeln; writeln('Fim.');
+    if n2 < n1 then
+    begin
+        for i := n2 to n1 do
+        begin
+            if ((i mod 2) <> 0) then // calc resto p determinar se é ímpar
+            begin
+                write(#9, i);
+            end;
+        end;
+    end;
+    
+    writeln; writeln; writeln('Fim.');
 end.    
