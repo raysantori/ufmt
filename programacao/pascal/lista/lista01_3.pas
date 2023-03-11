1bcfaff9d5d2311abd360afe0cdd123ac90fd3b1
raysantori <contato@raysantori.com>
2023-03-11 17:46:28 -0300

    fix lista01_3.pas

--- programacao/pascal/lista/lista01_3.pas
+++ programacao/pascal/lista/lista01_3.pas
@@ -1,27 +1,23 @@
 program lista01_3; // encontre uma solução aceitável para x^2-2=0 usando a fórmula iterativa
 
 var
-    x: array[0..4] of integer;
-    k, x0, xk: integer;
-    solution, iterativeFormula, stopTest, valueStopTest: real;
+    solution, stopTest, x0: real;
 
 begin // principal
-    stopTest:= 1;
-    valueStopTest:= 0.0001;
-    //stopTest:= (x[k1]-x[k])/(x[k1]);
-    //iterativeFormula:= ((sqr(x[k]))+2)/(2*x[k]);
+    x0:= 1; // anterior
+    solution:= (1/2)*(x0+(2/x0)); // atual
+    stopTest:= abs(solution-x0)/abs(solution);
 
-    for k:= 0 to 4 do
+    while stopTest > 0.0001 do
     begin
-      if stopTest > valueStopTest then
-      begin
-        x[0]:= 1; x0:= x[0];
-        xk:= x[k];
-        iterativeFormula:= ((sqr(xk))+2)/(2*xk);
-        solution:= iterativeFormula;
-      end;  
+        //writeln(solution:0:4, ' continua calculando...');
+        x0:= solution;
+        solution:= (1/2)*(x0+(2/x0)); // atual
+        stopTest:= abs(solution-x0)/abs(solution); // recálculo
     end;
-    writeln; writeln('Uma solução aceitável para x^2 + 2 = 0 é x = ', solution:0:4); writeln; 
-
-    writeln('Fim.')
+    
+    writeln; writeln('Uma solução aceitável para x^2 + 2 = 0 é x = ', solution:0:4);
+    
+    writeln; writeln('Fim.');
+    readln;
 end.
