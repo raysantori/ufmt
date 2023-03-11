64856fc9b1ba841ddf9d62178dbcd424516a5bff
raysantori <contato@raysantori.com>
2023-03-11 19:40:13 -0300

    feat 'gamificação' de naturalOdd.pas

--- programacao/pascal/list/naturalOdd.pas
+++ programacao/pascal/list/naturalOdd.pas
@@ -4,39 +4,41 @@ var
     odd, n1, n2: integer;
     info: char;
 
-begin // principal
+begin
     info:= 'y';
-    writeln; writeln('Número ímpares. Digite y para jogar.'); read(info);
+
+    writeln; writeln('Números naturais ímpares.');
+    write('Digite y para continuar. '); read(info);
     
-    while (info = 'y') do
+    while (info = 'y') do // loop principal
     begin
         writeln; write('Entre com N1: '); read(n1);
         write('Entre com N2: '); read(n2); writeln;
         
-        while (n1 > 0) and (n2 > 0) do
+        while (n1 > 0) and (n2 > 0) do // enquanto n1 e n2 naturais maiores que zero
         begin
             if n1 < n2 then // para N2 maior que N1
             begin
                 for odd := n1 to n2 do
                 begin
-                    if ((odd mod 2) <> 0) then // calc resto p determinar se é ímpar
+                    if ((odd mod 2) <> 0) then
                     begin
-                        write(#9, odd);
+                        write(#9, odd, ' ');
                     end;
                 end;
-            end;
-            if n2 < n1 then // para N1 maior que N2
+            end
+            else // para N1 maior que N2
             begin
                 for odd := n2 to n1 do
                 begin
-                    if ((odd mod 2) <> 0) then // calc resto p determinar se é ímpar
+                    if ((odd mod 2) <> 0) then
                     begin
-                        write(#9, odd);
+                        write(#9, odd, ' ');
                     end;
                 end;
             end;
             
-            writeln; writeln; writeln('Digite um número negativo para sair.');
+            writeln; writeln; writeln('Digite 0 para sair.');
             write('Entre com N1: '); read(n1);
             write('Entre com N2: '); read(n2); writeln;
         end;
@@ -44,5 +46,5 @@ begin // principal
         info:= 'n';
     end;
     
-    writeln; writeln; writeln('Fim de jogo.');
-end.    
+    writeln; writeln; writeln('Fim.');
+end.
