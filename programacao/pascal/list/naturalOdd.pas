51d668214a58321d1352f5fbe46a7957dd3035dd
raysantori <contato@raysantori.com>
2023-03-11 19:23:03 -0300

    feat continuidade naturalOdd.pas

--- programacao/pascal/list/naturalOdd.pas
+++ programacao/pascal/list/naturalOdd.pas
@@ -5,35 +5,44 @@ var
     info: char;
 
 begin // principal
-    write('Entre com N1: '); read(n1);
-    write('Entre com N2: '); read(n2); writeln;
+    info:= 'y';
+    writeln; writeln('Número ímpares. Digite y para jogar.'); read(info);
     
-    while (n1 > 0) and (n2 > 0) do
+    while (info = 'y') do
     begin
-        if n1 < n2 then // para N2 maior que N1
+        writeln; write('Entre com N1: '); read(n1);
+        write('Entre com N2: '); read(n2); writeln;
+        
+        while (n1 > 0) and (n2 > 0) do
         begin
-            for odd := n1 to n2 do
+            if n1 < n2 then // para N2 maior que N1
             begin
-                if ((odd mod 2) <> 0) then // calc resto p determinar se é ímpar
+                for odd := n1 to n2 do
                 begin
-                    write(#9, odd);
+                    if ((odd mod 2) <> 0) then // calc resto p determinar se é ímpar
+                    begin
+                        write(#9, odd);
+                    end;
                 end;
             end;
-        end;
-        if n2 < n1 then // para N1 maior que N2
-        begin
-            for odd := n2 to n1 do
+            if n2 < n1 then // para N1 maior que N2
             begin
-                if ((odd mod 2) <> 0) then // calc resto p determinar se é ímpar
+                for odd := n2 to n1 do
                 begin
-                    write(#9, odd);
+                    if ((odd mod 2) <> 0) then // calc resto p determinar se é ímpar
+                    begin
+                        write(#9, odd);
+                    end;
                 end;
             end;
+            
+            writeln; writeln; writeln('Digite um número negativo para sair.');
+            write('Entre com N1: '); read(n1);
+            write('Entre com N2: '); read(n2); writeln;
         end;
         
-        writeln; writeln; write('Entre com N1: '); read(n1);
-        write('Entre com N2: '); read(n2); writeln;
+        info:= 'n';
     end;
     
-    writeln; writeln; writeln('Fim.');
+    writeln; writeln; writeln('Fim de jogo.');
 end.    
